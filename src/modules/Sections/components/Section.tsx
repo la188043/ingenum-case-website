5276648aac0e99228d10bccc7d4716a3ac10183e
Sébastien Counen <sebastiencounen@Aureliens-MacBook-Pro.local>
2021-01-29 16:08:52 +0100

    Task styling

--- src/modules/Sections/components/Section.tsx
+++ src/modules/Sections/components/Section.tsx
@@ -1,12 +1,24 @@
 import { Section as SectionType } from '../models/Section.model';
+import { Task } from '../models/Task.model';
 interface Props {
   section: SectionType;
 }
 
 const Section = ({ section }: Props) => {
+  const tasks: Task[] = section.tasks;
+
   return (
     <div className="section">
-      <h2 className="heading-secondary underline">{section.name}</h2>
+      <h2 className="heading-secondary u-center-text underline">
+        {section.name}
+      </h2>
+      <div className="tasks-container u-mt-lg">
+        {tasks.map((task: Task) => (
+          <div className="task">
+            <p className="paragraph">{task.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
