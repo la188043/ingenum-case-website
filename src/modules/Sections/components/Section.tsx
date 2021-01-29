0f9527f670bf49a4da0f6d2747e1a151c6091f26
Sébastien Counen <sebastien.counen@outlook.com>
2021-01-29 22:54:08 +0100

    Deleted popup

--- src/modules/Sections/components/Section.tsx
+++ src/modules/Sections/components/Section.tsx
@@ -3,15 +3,12 @@ import React, { useState } from 'react';
 import { Section as SectionType } from '../models/Section.model';
 import { Task } from '../models/Task.model';
 
-import Popup from '../../shared/components/Popup';
 interface Props {
   section: SectionType;
 }
 
 const Section = ({ section }: Props) => {
   const tasks: Task[] = section.tasks;
-
-  const [isPopupVisible, setPopupVisible] = useState<boolean>(true);
 
   return (
     <div className="section">
@@ -21,12 +18,6 @@ const Section = ({ section }: Props) => {
       <div className="tasks-container u-mt-lg">
         {tasks.map((task: Task) => (
           <>
-            {isPopupVisible && (
-              <Popup>
-                <></>
-                {/* TODO Change: is being displayed due to map */}
-              </Popup>
-            )}
             <div className="task">
               <p className="paragraph">{task.name}</p>
             </div>
--- src/modules/shared/components/Popup.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-const Popup = ({ children }: PropsWithChildren<{}>): JSX.Element => {
-  return children as JSX.Element;
-};
-
-export default Popup;
