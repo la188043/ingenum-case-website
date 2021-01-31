42b1b7381381c5dfd3d66f529139a4dc198b9c64
Sébastien Counen <sebastien.counen@outlook.com>
2021-01-31 23:58:22 +0100

    Adding a section

--- src/modules/Sections/components/ManageSections.tsx
+++ src/modules/Sections/components/ManageSections.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import sectionService from '../services/section.service';
 
@@ -6,10 +6,15 @@ import { Section as SectionType } from '../models/Section.model';
 
 import Loading from '../../shared/components/Loading';
 import Section from './Section';
+import { AddSection } from '../models/AddSection.model';
+import Button from '../../shared/components/Button';
 
 const ManageSections = () => {
   const [sections, setSections] = useState<SectionType[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [formData, setFormData] = useState<AddSection>({ name: '' });
+
+  const formRef = useRef<HTMLFormElement>(null);
 
   const loadSections = async () => {
     setLoading(true);
@@ -22,8 +27,46 @@ const ManageSections = () => {
     loadSections();
   }, []);
 
+  const resetForm = () => formRef.current?.reset();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    setFormData({
+      ...formData,
+      [fieldName]: fieldValue.trim(),
+    });
+  };
+
+  const handleAddSection = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const newSection = await sectionService.addSection(formData);
+
+      setSections([...sections, newSection]);
+    } catch {
+      return;
+    }
+
+    resetForm();
+  };
+
   return (
     <Loading loading={loading}>
+      <form className="form u-mb-md" onSubmit={handleAddSection} ref={formRef}>
+        <h3 className="heading-tertiary">Ajouter une section</h3>
+        <input
+          className="form__input"
+          type="text"
+          name="name"
+          placeholder="Entrez le nom de la nouvelle section"
+          onChange={handleChange}
+        />
+
+        <Button type="submit" className="btn--primary" value="Ajouter" />
+      </form>
       <div className="sections-container">
         {sections.map((section: SectionType) => (
           <Section key={section.id} section={section} />
--- src/modules/Sections/services/section.service.ts
+++ src/modules/Sections/services/section.service.ts
@@ -1,4 +1,5 @@
 import httpService from '../../shared/services/http.service';
+import { AddSection } from '../models/AddSection.model';
 import { AddTask } from '../models/AddTask.model';
 
 import { Section } from '../models/Section.model';
@@ -8,7 +9,11 @@ const endpoint = '/tables';
 
 const getAll = (): Promise<Section[]> => httpService.get(endpoint);
 
+const addSection = (section: AddSection): Promise<Section> =>
+  httpService.post(endpoint, JSON.stringify(section));
+
 // eslint-disable-next-line
 export default {
   getAll,
+  addSection,
 };
