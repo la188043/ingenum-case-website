45c72e39ce5b62998305b4d07e41755de6f1f6ad
Sébastien Counen <sebastien.counen@outlook.com>
2021-02-01 19:44:25 +0100

    UpdateTask model and move task method service

--- src/modules/Sections/services/task.service.ts
+++ src/modules/Sections/services/task.service.ts
@@ -1,6 +1,7 @@
 import httpService from '../../shared/services/http.service';
 import { AddTask } from '../models/AddTask.model';
 import Task from '../models/Task.model';
+import { UpdateTask } from '../models/UpdateTask.model';
 
 const endpoint = '/tasks';
 
@@ -10,8 +11,12 @@ const addTask = (task: AddTask): Promise<Task> =>
 const deleteTask = (taskId: string): Promise<boolean> =>
   httpService.remove(`${endpoint}/${taskId}`);
 
+const moveTask = (taskId: string, table: UpdateTask): Promise<boolean> =>
+  httpService.put(`${endpoint}/move/${taskId}`, table);
+
 // eslint-disable-next-line
 export default {
   addTask,
   deleteTask,
+  moveTask,
 };
